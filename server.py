1ed64e2f9457b4dbc1f752e3a4471341814fd92e
Abhijit Kadalli <abhijitkadalli@nitgoa.ac.in>
2022-04-09 19:19:20 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,4 +1,20 @@
 import socket  
+import threading
 import sys
 import time
 
+s = socket.socket()
+print("socket was successfully created")
+
+port = 12345
+s.bind("", port)
+print("The socket binded to %s" %(port))
+
+s.listen(5)
+
+while True:
+    c, addrs = s.accept()
+    print("got connection from ", addrs)
+    c.send("Thank you, you have successfully connected to the server")
+    c.close()
+    break
